8d573cb73566a68b6734e0ec17f68780abaea348
Skimas <dranxman@gmail.com>
2017-06-17 22:03:25 +0300

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable connect do mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('59455d7f32a50f2d1484059a')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  db.collection('Users').find({
+    name: 'Simon'
+  }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users.');
+  });
+
+  //db.close();
+});
