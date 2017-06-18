99cbca1545cddf5a0503acd89ef460c4de8f6f9f
Skimas <dranxman@gmail.com>
2017-06-18 12:19:03 +0300

    refactor models add simple POST todos request

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.send(err);
+  });
 });
 
-var newTodo = new Todo({
-  text: 'Cook dinner',
-  completed: false,
-  completedAt: 20170212
-});
-
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save to do.');
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
